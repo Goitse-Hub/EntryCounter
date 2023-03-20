499c9fbdd4aac6727cab2e014db4a218bcad371f
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-03-20 21:35:07 +0200

    Add files via upload

---
+++ JavasVode.js
@@ -0,0 +1,96 @@
+// var num = document.getElementById("count")
+//        num.innerText = 6
+// let count = 0;
+
+// let bonusPoints = 0;
+// bonusPoints = 50
+// console.log(bonusPoints)
+// bonusPoints +=  50
+// console.log(bonusPoints)
+// bonusPoints -=  75
+// console.log(bonusPoints)
+// bonusPoints +=  45
+// console.log(bonusPoints)
+
+// function add(){
+//     var count = document.getElementById("count")
+//     for (count = 0; count <=30; count++){
+//         count++
+//         count.innerHTML = count
+//     }
+// }
+
+//  add()
+// function add(){
+    
+    
+// }
+
+// for (count = 5; count >= 1; count--){
+            
+//             console.log(count)
+            
+// }
+// console.log("Go")
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function laplogger(){
+//     let sum = 0
+//     sum = lap1 + lap2 + lap3
+//     console.log(sum)
+// }
+
+// laplogger()
+
+// let counter = 0
+
+// let count = 0
+
+// function numIncrementer(){
+//     count++
+// }
+
+// numIncrementer()
+// numIncrementer()
+// numIncrementer()
+
+// console.log(count)
+
+// function numLogger() {
+//     var num = 42
+//     console.log(num)
+// }
+
+// //call / invoke function
+// numLogger()
+
+
+
+
+
+// let name = "Scrimba"
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name
+
+// console.log(myGreeting)
+
+
+
+
+
+// function save(){
+//     alert(counter)
+// }
+
+
+// let message = "You have three new notifications"
+
+// // alert(message)
+
+
+// let messageToUser = message
+
+// alert(messageToUser)
---
+++ script.js
@@ -0,0 +1,8 @@
+
+let countElement = document.getElementById("count")
+let counter = 0
+function increment(){
+   counter++
+   countElement.innerText = counter      
+}
+
