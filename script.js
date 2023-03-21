e5c54cb88c4e8dd151a0fa2ebc7af174a87d59b9
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-03-21 14:20:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function increment(){
    countElement.innerText = counter      
 }
 
+let entryElemtn = document.getElementById("entries-el")
+let store = 0
+function save(){
+   store = " " + counter
+   entryElemtn.innerText += store + " - "
+}
+
+
